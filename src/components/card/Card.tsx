a411e1ada7ed8f00f270976816e8d066f0a7501c
CarinWood <carin.wood.85@gmail.com>
2024-02-22 10:38:10 +0100

    updated change task

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,9 +1,10 @@
-import { FC, useContext } from "react";
+import { FC, useContext, useState } from "react";
 import { FaRegTrashAlt } from "react-icons/fa";
 import { IoMdCheckmark } from "react-icons/io";
 import "./card.css";
 import { TodoContext } from "../../contexts/TodoContext";
 import { MdKeyboardArrowUp, MdKeyboardArrowDown } from "react-icons/md";
+import { BsPencil } from "react-icons/bs";
 
 type todoProps = {
   task: string;
@@ -13,6 +14,8 @@ type todoProps = {
 
 const Card: FC<todoProps> = ({ task, name, completed }) => {
   const { todos, setTodos } = useContext(TodoContext);
+  const [update, setUpdate] = useState(false);
+  const [inputValue, setInputValue] = useState(task);
 
   const deleteTodo = () => {
     const filteredTodos = todos.filter((todo) => todo.task !== task);
@@ -51,30 +54,62 @@ const Card: FC<todoProps> = ({ task, name, completed }) => {
     }
   };
 
-  return (
-    <div className="outer-card">
-      <section>
-        <MdKeyboardArrowUp className="arrow-up" onClick={moveTaskUp} />
-        <MdKeyboardArrowDown className="arrow-down" onClick={moveTaskDown} />
-      </section>
-      <div className="card">
-        <p className={completed ? "lined-through" : ""}>{task}</p>
+  const toggleUpdateCard = () => {
+    setUpdate(true);
+  };
 
-        <div className="bottom-card">
-          <p>
-            <i>{name}</i>
-          </p>
-          <div className="button-area">
-            <button onClick={markAsComplete}>
-              <IoMdCheckmark className="check-icon" />
-            </button>
-            <button onClick={deleteTodo}>
-              <FaRegTrashAlt className="trash-icon" />
-            </button>
+  const updateTask = () => {
+        setUpdate(false)
+        const updatedTodos = todos.map((todo) => {
+            if (todo.task === task) {
+              return { ...todo, task: inputValue };
+            }
+            return todo;
+          });
+          setTodos(updatedTodos);
+
+  }
+
+  return (
+    <>
+      {update && (
+        <section className="update-card">
+          <p className="update-text">Update task:</p>
+          <input
+            className="update-input"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <button className="update-button" onClick={updateTask}>Update</button>
+        </section>
+      )}
+      <div className="outer-card">
+        <section>
+          <MdKeyboardArrowUp className="arrow-up" onClick={moveTaskUp} />
+          <MdKeyboardArrowDown className="arrow-down" onClick={moveTaskDown} />
+        </section>
+        <div className="card">
+          <p className={completed ? "lined-through" : ""}>{task}</p>
+
+          <div className="bottom-card">
+            <p>
+              <i>{name}</i>
+            </p>
+            <div className="button-area">
+              <button onClick={toggleUpdateCard}>
+                <BsPencil className="pen-icon" />
+              </button>
+              <button onClick={markAsComplete}>
+                <IoMdCheckmark className="check-icon" />
+              </button>
+              <button onClick={deleteTodo}>
+                <FaRegTrashAlt className="trash-icon" />
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
